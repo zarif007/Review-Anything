a02bc9368fa16b5ad7f6cbcac8e76d48791fe82a
zarif007 <zarifhuq786@gmail.com>
2022-04-01 03:52:41 +0600

    Modified tranding part

--- review_anything/components/Trending.tsx
+++ review_anything/components/Trending.tsx
@@ -16,9 +16,9 @@ const Trending = () => {
       wrapper: `border-2 ${isDark ? 'border-gray-900 text-gray-300' : 'border-gray-200 text-gray-800'} mr-2`,
       trendingText: `font-bold text-2xl p-2`,
       trendingIcon: `h-10 w-6 sm:h-12 sm:w-8 ${isDark ? 'text-gray-200' : 'text-gray-800'} iconAnimation pt-1`,
-      postWrapper: `flex flex-wrap space-x-2 items-centre m-2`,
+      postWrapper: `flex flex-col p-2 pb-1`,
       postImg: `h-10 w-10 sm:h-12 sm:w-12 p-2 rounded-full cursor-pointer pt-2`,
-      postDetailesWrapper: `flex flex-col`,
+      postDetailesWrapper: `flex items-center space-x-1  lg:space-x-2 font-semibold md:text-xs lg:text-sm`,
       genre: `bg-blue-500 hover:bg-blue-600 p-2 m-1 mt-0 rounded-2xl font-semibold text-xs iconAnimation hover:text-white text-white`,
   }
 
@@ -47,21 +47,19 @@ const Trending = () => {
         {
             posts.slice(0, 3).map((post: postInterface) => {
                 return (
-                    <div className={styles.postWrapper} key={post.id}>
+                    <div className={styles.postWrapper}>
+                      <div className='flex space-x-2 items-center pb-2'>
                         <img src={post.img} className={styles.postImg} />
-                        <div className={styles.postDetailesWrapper}>
-                            <p className='font-semibold text-lg text-blue-400'>{post.title}</p>
-                            <div className='flex space-x-3 items-center pt-1 flex-wrap font-semibold'>
-                                <div className='flex space-x-1'>
-                                    <p>{post.rating} </p>
-                                    <BsFillStarFill className='text-yellow-500 h-6' />
-                                </div>
-                                <div className={styles.genre}>
-                                    {post.genre}
-                                </div>
-                                <p>{post.review.slice(0, 18)}...</p>
-                            </div>
+                        <p className='font-semibold text-blue-600 text-lg'>{post.title}</p>
+                      </div>
+                      <div className={styles.postDetailesWrapper}>
+                        <div className={styles.genre}>{post.genre}</div>
+                        <div className='flex space-x-0 lg:space-x-1 items-center'>
+                          <p className=''>{post.rating}</p>
+                          <BsFillStarFill className='text-yellow-500 h-6' />
                         </div>
+                        <p>{post.review.slice(0, 18)}...</p>
+                      </div>
                     </div>
                 )
             })
