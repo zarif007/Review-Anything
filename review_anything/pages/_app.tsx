2686fc0ed5b45e3954e8a4c78a9115825c42326a
zarif007 <zarifhuq786@gmail.com>
2022-05-18 20:49:33 +0600

    Added nprogress

--- review_anything/pages/_app.tsx
+++ review_anything/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import '../styles/nprogress.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil'
+import nProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 
